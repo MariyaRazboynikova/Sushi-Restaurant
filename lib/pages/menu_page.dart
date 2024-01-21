e06ab28fb071b755fccfd46d23d95cc0b3df0ef9
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-01-21 19:18:46 +0500

    menu changes

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi_restaurant/components/button.dart';
+import 'package:sushi_restaurant/themes/colors.dart';
+
+import '../components/drawer.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({
@@ -14,43 +18,64 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 155, 38, 30),
-        title: Text(
-          'Menu',
-          style: GoogleFonts.dmSerifDisplay(
-            fontSize: 15,
-            color: Colors.white,
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: Icon(Icons.menu, color: Colors.grey[900]),
+          title: Text(
+            'Menu',
+            style: GoogleFonts.dmSerifDisplay(
+              fontSize: 15,
+              color: Colors.grey[900],
+            ),
           ),
         ),
-      ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 25),
-
-          //shop subtitle
-          Center(
-            child: Text(
-              "Pick from selected list of premium products",
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.inversePrimary,
+        drawer: MyDrawer(),
+        body: Column(
+          children: [
+            //promo banner
+            Container(
+              decoration: BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.circular(20),
               ),
-            ),
-          ),
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              padding: const EdgeInsets.all(25),
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      //text message
+                      Text(
+                        'Скидка 30% на первый заказ',
+                        style: GoogleFonts.dmSerifDisplay(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
 
-          //product list
-          SizedBox(
-            height: 550,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.all(15),
-              itemBuilder: (context, index) {
-                //get each individual product  from shop
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+                      const SizedBox(height: 20),
+
+                      //redeem button
+                      MyButton(
+                        text: 'Активация',
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+
+                  //image
+                ],
+              ),
+            )
+
+            //search bar
+
+            //menu list
+
+            //popular food
+          ],
+        ));
   }
 }
