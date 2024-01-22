79627e61f750fc9be2ffe2d51711e11c7e218d0c
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-01-22 10:38:40 +0500

    menu list

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sushi_restaurant/components/button.dart';
+import 'package:sushi_restaurant/models/food_model.dart';
 import 'package:sushi_restaurant/themes/colors.dart';
 
 import '../components/drawer.dart';
+import '../components/food_tile.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({
@@ -15,73 +17,132 @@ class MenuPage extends StatefulWidget {
 }
 
 class _MenuPageState extends State<MenuPage> {
+  //food menu
+  List foodMenu = [
+    //salmon sushi
+    Food(
+      name: 'Суши с лососем',
+      price: '250.00',
+      imagePath: 'lib/images/free-icon-sushi-2713959.png',
+      rating: '4,9',
+    ),
+
+    //кшсу
+    Food(
+      name: 'Рис',
+      price: '150.00',
+      imagePath: 'lib/images/free-icon-rice-bowl-2714036.png',
+      rating: '4,7',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          leading: Icon(Icons.menu, color: Colors.grey[900]),
-          title: Text(
-            'Menu',
-            style: GoogleFonts.dmSerifDisplay(
-              fontSize: 15,
-              color: Colors.grey[900],
-            ),
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.menu, color: Colors.grey[900]),
+        title: Text(
+          'Menu',
+          style: GoogleFonts.dmSerifDisplay(
+            fontSize: 15,
+            color: Colors.grey[900],
           ),
         ),
-        drawer: MyDrawer(),
-        body: Column(
-          children: [
-            //promo banner
-            Container(
-              decoration: BoxDecoration(
-                color: primaryColor,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              margin: const EdgeInsets.symmetric(horizontal: 25),
-              padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //text message
-                      Text(
-                        'Скидка 30% на первый заказ',
-                        style: GoogleFonts.dmSerifDisplay(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
+      ),
+      drawer: MyDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //promo banner
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //text message
+                    Text(
+                      'Скидка 30% на первый заказ',
+                      style: GoogleFonts.dmSerifDisplay(
+                        color: Colors.white,
+                        fontSize: 20,
                       ),
+                    ),
 
-                      const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                      //redeem button
-                      MyButton(
-                        text: 'Активация',
-                        onTap: () {},
-                      ),
-                    ],
-                  ),
+                    //redeem button
+                    MyButton(
+                      text: 'Активация',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
 
-                  //image
-                  Image.asset(
-                    'lib/images/free-icon-sushi-2713959.png',
-                    height: 100,
-                  ),
-                ],
+                //image
+                Image.asset(
+                  'lib/images/free-icon-ramen-2714030.png',
+                  height: 100,
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          //search bar
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
-            )
+            ),
+          ),
 
-            //search bar
+          const SizedBox(height: 25),
 
-            //menu list
+          //menu list
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Text('Меню',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[800],
+                    fontSize: 18)),
+          ),
 
-            //popular food
-          ],
-        ));
+          const SizedBox(height: 10),
+
+          //popular food
+          Expanded(
+            child: ListView.builder(
+              itemCount: foodMenu.length,
+              itemBuilder: (context, index) => FoodTile(
+                food: foodMenu[index],
+              ),
+            ),
+          ),
+          //popular food
+        ],
+      ),
+    );
   }
 }
