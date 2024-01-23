b55bd5d09c715c71af4efe8d2298a5e06d68bac2
MariyaRazboynikova <mariya.razboynikova@gmail.com>
2024-01-23 10:25:12 +0500

    add to cart and remove from cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sushi_restaurant/components/button.dart';
 import 'package:sushi_restaurant/models/food_model.dart';
 import 'package:sushi_restaurant/models/shop.dart';
 import 'package:sushi_restaurant/themes/colors.dart';
@@ -8,54 +9,84 @@ class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   //remove from cart
-  void removeFromCart() {}
+  void removeFromCart(Food food, BuildContext context) {
+    // get access to shop
+    final shop = context.read<Shop>();
+
+    //remove from cart
+    shop.removeCart(food);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<Shop>(
       builder: (context, value, child) => Scaffold(
+        backgroundColor: primaryColor,
         appBar: AppBar(
           backgroundColor: primaryColor,
+          elevation: 0,
           title: const Text('Корзина'),
         ),
-        body: ListView.builder(
-          itemCount: value.cart.length,
-          itemBuilder: (context, index) {
-            //get food from cart
-            final Food food = value.cart[index];
+        body: Column(
+          children: [
+            //CUSTOMER CART
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: (context, index) {
+                  //get food from cart
+                  final Food food = value.cart[index];
 
-            //get food name
-            final String foodName = food.name;
+                  //get food name
+                  final String foodName = food.name;
 
-            //get food price
-            final String foodPrice = food.price;
+                  //get food price
+                  final String foodPrice = food.price;
 
-            //return list tile
-            return Container(
-              decoration: BoxDecoration(color: secondaryColor),
-              margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
-              child: ListTile(
-                title: Text(
-                  foodName,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                  foodPrice,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete_outline),
-                  onPressed: removeFromCart,
-                ),
+                  //return list tile
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: ListTile(
+                      title: Text(
+                        foodName,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        foodPrice,
+                        style: TextStyle(
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.delete_outline,
+                          color: Colors.grey[300],
+                        ),
+                        onPressed: () => removeFromCart(food, context),
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+
+            //PAY BUTTON
+
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MyButton(
+                text: "Оплатить сейчас",
+                onTap: () {},
+              ),
+            ),
+          ],
         ),
       ),
     );
